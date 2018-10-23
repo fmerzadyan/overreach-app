44e6b163968ee54c93b01ffe0a45355e5acacb77
Frankie <fmerzadyan@axway.com>
2018-10-24 00:56:20 +0200

    Made map more interactive.

--- src/scripts/components/WorldMap.js
+++ src/scripts/components/WorldMap.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { ComposableMap, ZoomableGroup, Geographies, Geography, Marker } from 'react-simple-maps';
+import {
+    ComposableMap,
+    ZoomableGroup,
+    Geographies,
+    Geography,
+    Marker
+} from 'react-simple-maps';
 import Markers from 'react-simple-maps/lib/Markers';
+import { scaleLinear } from 'd3-scale';
+import geoJsonObject from '../../assets/maps/world-50m-simplified.json';
 
 export default class WorldMap extends React.Component {
     state = {
-        zoom: 2
+        zoom: 2,
+        defaultCentre: [6.15, 47.3],
+        currentCentre: [], // TODO
+        markers: [] // TODO
     };
     handleZoomIn = () => {
         this.setState(() => ({
@@ -15,23 +26,47 @@ export default class WorldMap extends React.Component {
         this.setState(() => ({
             zoom: this.state.zoom / 2
         }));
-    }
+    };
+    handleMarkerClick = (marker, e) => {
+        console.log(marker);
+    };
+    handleMoveStart = (currentCentre = this.state.defaultCentre) => {
+        console.log(currentCentre); // TODO
+    };
+    handleMoveEnd = (newCentre) => {
+        console.log(newCentre); // TODO
+    };
     render() {
-        // Requires map json file to be in dist/ folder.
-        const mapPath = './world-110m.json';
+        const colorScale = scaleLinear()
+            .domain([0, 100000000, 1338612970]) // Max is based on China
+            .range(['#FFF176', '#FFC107', '#E65100']);
         return (
             <div>
                 <button onClick={this.handleZoomIn}>{'Zoom in'}</button>
                 <button onClick={this.handleZoomOut}>{'Zoom out'}</button>
                 <hr />
-                <ComposableMap style={{ width: "100%" }}>
-                    <ZoomableGroup zoom={this.state.zoom} center={[8.5, 47.3]}>
-                        <Geographies geography={mapPath}>
-                            {(geographies, projection) => geographies.map(geography => (
+                <ComposableMap style={{ width: '100%' }}>
+                    <ZoomableGroup
+                        zoom={this.state.zoom}
+                        center={this.state.defaultCentre}
+                        onMoveStart={this.handleMoveStart}
+                        onMoveEnd={this.handleMoveEnd}
+                    >
+                        <Geographies geography={geoJsonObject} disableOptimization>
+                            {(geographies, projection) => geographies.map((geography, i) => (
                                 <Geography
-                                    key={geography.id}
+                                    key={`geography-${i}`}
+                                    cacheId={`geography-${i}`}
                                     geography={geography}
                                     projection={projection}
+                                    style={{
+                                        default: {
+                                            fill: colorScale(geography.properties.pop_est),
+                                            stroke: '#FFF',
+                                            strokeWidth: 0.5,
+                                            outline: 'none',
+                                        },
+                                    }}
                                 />
                             ))}
                         </Geographies>
@@ -39,11 +74,12 @@ export default class WorldMap extends React.Component {
                             <Marker
                                 marker={{ coordinates: [8.5, 47.3] }}
                                 style={{
-                                    default: { fill: "yellow" },
-                                    hover: { fill: "orange" },
-                                    pressed: { fill: "red" },
-                                }
-                                }>
+                                    default: { fill: 'yellow' },
+                                    hover: { fill: 'orange' },
+                                    pressed: { fill: 'red' }
+                                }}
+                                onClick={this.handleMarkerClick}
+                            >
                                 <circle cx={0} cy={0} r={5} />
                             </Marker>
                         </Markers>
@@ -52,4 +88,4 @@ export default class WorldMap extends React.Component {
             </div>
         );
     }
-};
+}
